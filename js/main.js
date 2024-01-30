c074bd08e2fa71b6067a3b4d229ef122b0ceec47
Mariela Altieri <marie88me81@gmail.com>
2024-01-29 21:17:21 -0300

    Proyecto JS - Mariela Altieri

---
+++ js/main.js
@@ -0,0 +1,43 @@
+let saludar = "¡Bienvenidos a Compras Controladas!"
+console.log(saludar)
+alert(saludar)
+
+let nombre = prompt("Ingresa tu nombre:")
+console.log("Hola", nombre)
+alert("Hola"+ " " +nombre)
+
+let carrito = [];
+let totalGasto = 0;
+let presupuesto = parseFloat(prompt("Ingrese su presupuesto:"));
+
+while (presupuesto > totalGasto) {
+    let producto = prompt("Ingrese el nombre del producto:");
+    let precio = parseFloat(prompt("Ingrese el precio del producto:"));
+
+    carrito.push({ producto, precio });
+
+    totalGasto += precio;
+
+    console.log("Carrito de compras:");
+    for (let i = 0; i < carrito.length; i++) {
+        console.log(`${carrito[i].producto}: $${carrito[i].precio}`);
+    }
+    console.log(`Total de gasto: $${totalGasto}`);
+
+    let continuar = prompt("¿Desea agregar más productos? (Sí/No)").toLowerCase();
+    if (continuar !== 'si') {
+        break;
+    }
+}
+
+console.log("Resumen final:");
+for (let i = 0; i < carrito.length; i++) {
+    console.log(`${carrito[i].producto}: $${carrito[i].precio}`);
+}
+console.log(`Total de gasto: $${totalGasto}`);
+
+if (totalGasto > presupuesto) {
+    alert("¡Has excedido tu presupuesto!" + " " + "El monto alcanzado es de:" + " " + totalGasto);
+} else {
+    alert("¡Gracias por su compra!" + " " + "El monto total es de:" + " " + totalGasto);
+}
